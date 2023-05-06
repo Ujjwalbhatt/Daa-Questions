8af7af9bcb9d77b3605a6fcb9a71c886ec6475a4
Ujjwal Bhatt <ujjwalbhatt04@gmail.com>
2023-05-06 10:27:21 +0530

    mater

---
+++ Week 6/Question1.cpp
@@ -0,0 +1,19 @@
+/*Given a (directed/undirected) graph, design an algorithm and implement it using a program to
+find if a path exists between two given vertices or not. (Hint: use DFS)*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int cmp_count = 0;
+int swap_count = 0;
+
+
+
+
+
+
+                        
+
+
+
